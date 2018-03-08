25159ccc967792ba4c3a3f5849eb67ff4b5d2ecf
Christine <cchen7196@gmail.com>
2018-03-08 14:51:12 -0500

    fix test

--- openstack/test/test_openstack.py
+++ openstack/test/test_openstack.py
@@ -297,15 +297,16 @@ class TestOpenstack(AgentCheckTest):
             scope = self.check.ensure_auth_scope(instance)
 
             self.assertEqual(self.check.get_scope_for_instance(instance), scope)
-            self.check._send_api_service_checks(scope)
+            self.check._send_api_service_checks(scope, ['optional:tag1'])
 
             self.service_checks = self.check.get_service_checks()
-            tags = ['optional:tag1']
+            #tags = ['optional:tag1']
             # Expect OK, since we've mocked an API response
-            self.assertServiceCheck(self.check.IDENTITY_API_SC, status=AgentCheck.OK, count=1, tags=tags)
+            self.assertServiceCheck(self.check.IDENTITY_API_SC, status=AgentCheck.OK, count=1)
 
             # Expect CRITICAL since URLs are non-existent
-            self.assertServiceCheck(self.check.COMPUTE_API_SC, status=AgentCheck.CRITICAL, count=1, tags=tags)
+            self.assertServiceCheck(self.check.COMPUTE_API_SC, status=AgentCheck.CRITICAL, count=1
+                )
             self.assertServiceCheck(self.check.NETWORK_API_SC, status=AgentCheck.CRITICAL, count=1)
 
             self.check._current_scope = scope
